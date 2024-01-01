a141cf4f03deaf32a5446095eb660484a9d3d1e7
Anita <151672361+Pazzacattiva@users.noreply.github.com>
2024-01-01 16:58:21 +0100

    Add files via upload

---
+++ src/Assignments/Resizing/app.cpp
@@ -0,0 +1,165 @@
+//
+// Created by pbialas on 25.09.2020.
+//
+
+#include "app.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/constants.hpp"
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include "glm/gtc/matrix_transform.hpp"
+#include "Application/utils.h"
+
+
+void SimpleShapeApplication::init() {
+
+    auto program = xe::utils::create_program(
+            {{GL_VERTEX_SHADER,   std::string(PROJECT_DIR) + "/shaders/base_vs.glsl"},
+             {GL_FRAGMENT_SHADER, std::string(PROJECT_DIR) + "/shaders/base_fs.glsl"}});
+
+    if (!program) {
+        std::cerr << "Invalid program" << std::endl;
+        exit(-1);
+    }
+    // Usuwanie powtarzających się wierzchołków w trójkątach
+    std::vector<GLushort> indices = {0,3,2 , 1,3,0 , 4,5,6 , 7,8,9 , 10,11,12 , 13,14,15};
+
+    // // Wierzchołki i kolory piramidy
+    std::vector<GLfloat> vertices = {
+            0.0f, 0.0f, 0.0f, 0.0f,1.0f,0.0f,//0
+            1.0f, 0.0f, 0.0f, 0.0f,1.0f,0.0f,//1
+            0.0f, 1.0f, 0.0f, 0.0f,1.0f,0.0f,//2
+            1.0f, 1.0f, 0.0f, 0.0f,1.0f,0.0f,//3
+
+            0.0f, 0.0f, 0.0f, 1.0f,0.0f,0.0f,
+            0.0f, 1.0f, 0.0f, 1.0f,0.0f,0.0f,
+            0.5f, 0.5f, 1.0f, 1.0f,0.0f,0.0f,//6
+
+            0.0f, 1.0f, 0.0f, 0.0f,0.0f,1.0f,
+            1.0f, 1.0f, 0.0f, 0.0f,0.0f,1.0f,
+            0.5f, 0.5f, 1.0f, 0.0f,0.0f,1.0f,//9
+
+            1.0f, 1.0f, 0.0f, 0.5f,0.0f,0.5f,
+            1.0f, 0.0f, 0.0f, 0.5f,0.0f,0.5f,
+            0.5f, 0.5f, 1.0f, 0.5f,0.0f,0.5f,//12
+
+            1.0f, 0.0f, 0.0f, 0.0f,0.5f,0.5f,
+            0.0f, 0.0f, 0.0f, 0.0f,0.5f,0.5f,
+            0.5f, 0.5f, 1.0f, 0.0f,0.5f,0.5f}; //15
+
+    // Modyfikowanie koloru pikseli
+    float strength=0.6;
+    float color[3]={0.7,0.6,0.7};
+
+    //Resizng
+        int width, height;
+        std::tie(width, height) = frame_buffer_size();
+
+        fov_ = 45.0f;
+        aspect_ = (float)width / (float)height;
+        near_ = 0.1f;
+        far_ = 100.0f;
+
+        glm::mat4 projection = glm::perspective(fov_, aspect_, near_, far_);
+        P_ = projection;
+        glm::mat4 camera  = glm::lookAt(glm::vec3(10.6, -20.0, 6.6), glm::vec3(-0.5, 0.1, 0.0), glm::vec3(0.0, 0.3, -0.25));
+        V_= camera;
+
+        glGenBuffers(1, &u_pvm_buffer_);
+        glBindBuffer(GL_UNIFORM_BUFFER, u_pvm_buffer_);
+        glBufferData(GL_UNIFORM_BUFFER, sizeof(glm::mat4), &P_, GL_STATIC_DRAW);
+        glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+    // PVM
+    glm::mat4 PVM_matrix(1.0f);
+    glm::mat4 perspective_matrix = glm::perspective(25.0, 1.5, 50.0, 3.0);
+    glm::mat4 look_matrix = glm::lookAt(glm::vec3(10.6, -20.0, 6.6), glm::vec3(-0.5, 0.1, 0.0), glm::vec3(0.0, 0.3, -0.25));
+    glm::mat4 PVM = PVM_matrix*perspective_matrix*look_matrix;
+
+    GLuint v_buffer_handle_uniforms;
+    glGenBuffers(1, &v_buffer_handle_uniforms);
+    glBindBuffer(GL_UNIFORM_BUFFER, v_buffer_handle_uniforms);
+    glBufferData(GL_UNIFORM_BUFFER, 8 * sizeof(GLfloat), nullptr, GL_STATIC_DRAW);
+    glBindBufferBase(GL_UNIFORM_BUFFER, 0, v_buffer_handle_uniforms);
+
+    glBufferSubData(GL_UNIFORM_BUFFER, 0,sizeof(float),&strength);
+    glBufferSubData(GL_UNIFORM_BUFFER, 4*sizeof(float),3*sizeof(float),color);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+
+    GLuint v_buffer_handle_uniforms_move;
+    glGenBuffers(1, &v_buffer_handle_uniforms_move);
+    glBindBuffer(GL_UNIFORM_BUFFER, v_buffer_handle_uniforms_move);
+    glBufferData(GL_UNIFORM_BUFFER, 16 * sizeof(GLfloat), nullptr, GL_STATIC_DRAW);
+    glBufferSubData(GL_UNIFORM_BUFFER, 0, 16 * sizeof(float), &PVM);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+    // Generowanie bufora i ładowanie do niego danych wierzchołków
+    GLuint v_buffer_handle;
+    glGenBuffers(1, &v_buffer_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    //Bufor indeksów
+    GLuint v_buffer_handle_indices;
+    glGenBuffers(1, &v_buffer_handle_indices);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, v_buffer_handle_indices);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLfloat), indices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    glGenVertexArrays(1, &vao_);
+    glBindVertexArray(vao_);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, v_buffer_handle_indices);
+
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3*sizeof(GLfloat)));
+
+    glBindVertexArray(0);
+    glBindBufferBase(GL_UNIFORM_BUFFER,0,v_buffer_handle_uniforms);
+    glBindBufferBase(GL_UNIFORM_BUFFER,1,v_buffer_handle_uniforms_move);
+
+    // Przezroczyste tło
+    glClearColor(0.81f, 0.81f, 0.8f, 1.0f);
+
+    int w, h;
+    std::tie(w, h) = frame_buffer_size();
+
+    glViewport(0, 0, w, h);
+
+    glUseProgram(program);
+}
+   void SimpleShapeApplication::framebuffer_resize_callback(int w, int h) {
+    Application::framebuffer_resize_callback(w, h);
+    glViewport(0, 0, w, h);
+    aspect_ = (float)w / h;
+    P_ = glm::perspective(fov_, aspect_, near_, far_);
+    V_ = glm::lookAt(glm::vec3(10.6, -20.0, 6.6), glm::vec3(-0.5, 0.1, 0.0), glm::vec3(0.0, 0.3, -0.25));
+
+}
+
+void SimpleShapeApplication::frame() {
+
+    //Resizng
+    auto PVM = P_ * V_;
+    glBindBuffer(GL_UNIFORM_BUFFER, u_pvm_buffer_);
+    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), &PVM[0]);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+    //Pyramid
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+
+    // Binding the VAO - skonfiguruje wszystkie wymagane bufory wierzchołków
+    glBindVertexArray(vao_);
+
+    //Rysowanie za pomocą indeksów
+    glDrawElements(GL_TRIANGLES , 18, GL_UNSIGNED_SHORT, reinterpret_cast<GLvoid *>(0));
+    glBindVertexArray(0);
+}
