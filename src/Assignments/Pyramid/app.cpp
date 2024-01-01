0bcd85e135893956cc21746dbe203d8ef8531abb
Anita <151672361+Pazzacattiva@users.noreply.github.com>
2024-01-01 18:35:36 +0100

    Update app.cpp

--- src/Assignments/Pyramid/app.cpp
+++ src/Assignments/Pyramid/app.cpp
@@ -22,7 +22,7 @@ void SimpleShapeApplication::init() {
         std::cerr << "Invalid program" << std::endl;
         exit(-1);
     }
-    // Usuwanie powtarzających się wierzchołków w trójkątach
+    // Usuwanie powtarzających się wierzchołków 
     std::vector<GLushort> indices = {0,3,2 , 1,3,0 , 4,5,6 , 7,8,9 , 10,11,12 , 13,14,15};
 
     // // Wierzchołki i kolory piramidy
