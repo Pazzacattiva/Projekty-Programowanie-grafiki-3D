7de6664c94b7637a2939740cb909b76228ee9bdd
Anita <151672361+Pazzacattiva@users.noreply.github.com>
2024-01-09 15:09:09 +0100

    Add files via upload

---
+++ src/Assignments/Textures/app.h
@@ -0,0 +1,61 @@
+//
+// Created by pbialas on 05.08.2020.
+//
+
+#pragma once
+
+#include <vector>
+
+#include "Application/application.h"
+#include "Application/utils.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/constants.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glad/gl.h"
+#include "camera.h"
+#include "cameracontroler.h"
+#include "Engine/Mesh.h"
+
+class SimpleShapeApplication : public xe::Application
+{
+public:
+
+    void set_camera(Camera *camera) { camera_ = camera; }
+    Camera *camera() const { return camera_; }
+
+    SimpleShapeApplication(int width, int height, std::string title, bool debug) : Application(width, height, title, debug) {}
+
+    void init() override;
+
+    void frame() override;
+
+    void framebuffer_resize_callback(int w, int h) override;
+
+    void scroll_callback(double xoffset, double yoffset) override;
+
+    void set_controler(CameraControler *controler) { controler_ = controler; }
+    void cursor_position_callback(double x, double y);
+    void mouse_button_callback(int button, int action, int mods);
+
+    void add_submesh(xe::Mesh *mesh) {
+        meshes_.push_back(mesh);
+    }
+private:
+    GLuint vao_;
+
+    glm::mat4 M_;
+
+    GLuint u_pvm_buffer_;
+
+    Camera *camera_;
+
+    CameraControler *controler_;
+
+    std::vector<xe::Mesh*> meshes_;
+
+    GLuint color_uniform_buffer_ = 0u;
+
+    GLint  uniform_map_Kd_location_ = 0;
+
+
+};
